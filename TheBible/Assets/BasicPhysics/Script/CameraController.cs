a516f7a130152cee495cf26fb8b883e77602a33c
dongho5309 <dongho5309@naver.com>
2020-01-09 22:38:50 +0900

    Unity init commit + add Physics Exmaple

---
+++ TheBible/Assets/BasicPhysics/Script/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private float moveWeight;
+    [SerializeField]
+    private float rotationWeight;
+
+    Vector3 initPos;
+    float degree;
+    float moveUp;
+    float moveSide;
+    float moveForward;
+
+    private void Start()
+    {
+        initPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            moveUp += Input.GetAxis("Vertical") * Time.deltaTime * moveWeight;
+            degree += Input.GetAxis("Horizontal") * Time.deltaTime * rotationWeight;
+        }
+        else
+        {
+            moveForward += Input.GetAxis("Vertical") * Time.deltaTime * moveWeight;
+            moveSide += Input.GetAxis("Horizontal") * Time.deltaTime * moveWeight;
+        }
+        transform.rotation = Quaternion.AngleAxis(degree, transform.up);
+        transform.position = initPos + moveForward * transform.forward + moveSide * transform.right + moveUp * transform.up;
+    }
+}
