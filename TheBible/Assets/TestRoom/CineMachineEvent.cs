d08f1f05ccef1eb77cef0978841c486cdc934b94
dongho5309 <dongho5309@naver.com>
2020-01-11 13:05:39 +0900

    Update CineMachine simple Example made by dongho

---
+++ TheBible/Assets/TestRoom/CineMachineEvent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CineMachineEvent : MonoBehaviour
+{
+    public PlayableDirector playableDirector;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log($"{collision.gameObject.name} : Tag is {collision.gameObject.tag}");
+        if (collision.gameObject.CompareTag("Trigger"))
+        {
+            playableDirector.Play();
+        }
+    }
+}
