8090c8f2f5823d0e292f2a2537b083f49b9bbb83
dongho5309 <dongho5309@naver.com>
2020-01-13 22:47:12 +0900

    MemoryPool 예제추가 -> 리팩토링예정

---
+++ TheBible/Assets/Examples/MemoryPool/CsMissile.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CsMissile : MonoBehaviour {
+
+    int speed = 10;
+    
+	// Update is called once per frame
+	void Update () {
+        float amtMove = speed * Time.smoothDeltaTime;
+        transform.Translate(Vector3.forward * amtMove); 
+	}
+}
