3187725be8d31312669d016d6eeda846a7832f27
dongho5309 <dongho5309@naver.com>
2020-01-19 21:25:31 +0900

    폰트 파일 lfs 트랙킹, IntroScene에서 E버튼 누르고 있으면 360도 프로그레스바 회전

---
+++ TheBible/Assets/Scripts/IntroEvents.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroEvents : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Debug.Log("게임 시작!");
+    }
+    public void ExitGame()
+    {
+        Debug.Log("게임 끝!");
+        Application.Quit();
+    }
+}
