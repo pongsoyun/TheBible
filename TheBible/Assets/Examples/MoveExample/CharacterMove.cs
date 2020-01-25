50e4720c33a3687bcbd70bdd4fdb6d0173849dd0
dongho5309 <dongho5309@naver.com>
2020-01-25 11:27:04 +0900

    Add CharacterMove 나중에 Cinematic Machine 추가해서 카메라 작업

---
+++ TheBible/Assets/Examples/MoveExample/CharacterMove.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour
+{
+    public SpriteRenderer chracter;
+    private float moveWeight;
+    [SerializeField]
+    private float runSpeed;
+    [SerializeField]
+    private float walkSpeed;
+    Vector3 initPos;
+    float moveSide;
+    float moveForward;
+
+    private void Start()
+    {
+        initPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            moveWeight = runSpeed;
+        }
+        else
+        {
+            moveWeight = walkSpeed;
+        }
+        moveForward += Input.GetAxis("Vertical") * Time.deltaTime * moveWeight;
+        moveSide += Input.GetAxis("Horizontal") * Time.deltaTime * moveWeight;
+    
+        if(Input.GetAxis("Horizontal") < 0)
+        {
+            chracter.flipX = true;
+        }
+        else
+        {
+             chracter.flipX = false;
+        }
+
+        transform.position = initPos + moveForward * transform.up + moveSide * transform.right;
+    }
+}
