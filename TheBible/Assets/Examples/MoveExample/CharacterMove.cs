d67a52ca33dd37ef26929c40852492f5797144d6
dongho5309 <dongho5309@naver.com>
2020-01-25 16:10:01 +0900

    Event Example added at MoveExample

--- TheBible/Assets/Examples/MoveExample/CharacterMove.cs
+++ TheBible/Assets/Examples/MoveExample/CharacterMove.cs
@@ -5,22 +5,37 @@ using UnityEngine;
 public class CharacterMove : MonoBehaviour
 {
     public SpriteRenderer chracter;
-    private float moveWeight;
+
     [SerializeField]
     private float runSpeed;
     [SerializeField]
     private float walkSpeed;
+
+    private float moveWeight;
     Vector3 initPos;
     float moveSide;
     float moveForward;
+
+    public delegate void TestEvent();
+    public event TestEvent ActivateEvent;
 
     private void Start()
     {
         initPos = transform.position;
     }
+    
 
     // Update is called once per frame
     void Update()
+    {
+        MovingCharacter();
+        if(Input.GetKey(KeyCode.E))
+        {
+            ActivateEvent?.Invoke();
+        }
+    }
+
+    void MovingCharacter()
     {
         if (Input.GetKey(KeyCode.Space))
         {
@@ -32,12 +47,12 @@ public class CharacterMove : MonoBehaviour
         }
         moveForward += Input.GetAxis("Vertical") * Time.deltaTime * moveWeight;
         moveSide += Input.GetAxis("Horizontal") * Time.deltaTime * moveWeight;
-    
+
         if(Input.GetAxis("Horizontal") < 0)
         {
             chracter.flipX = true;
         }
-        else
+        else if(Input.GetAxis("Horizontal") > 0)
         {
              chracter.flipX = false;
         }
