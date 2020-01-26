8334e8aa631ddc40553d2ca2b51cf7963aadc3f0
pongsoyun <thdbstjdud@gmail.com>
2020-01-26 20:16:08 +0900

    InteractionExample Test

--- TheBible/Assets/Scripts/InteractionExample.cs
+++ TheBible/Assets/Scripts/InteractionExample.cs
@@ -11,11 +11,11 @@ public class InteractionExample : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.E))
+        if (Input.GetKey(KeyCode.E))
         {
             FilledImage.fillAmount += 0.01f;
         }
-        if(FilledImage.fillAmount >= 1.0f)
+        if (FilledImage.fillAmount >= 1.0f)
         {
             Debug.Log("FilledImage Reset 1.0 to 0!");
             FilledImage.fillAmount = 0;
