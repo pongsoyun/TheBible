128ce3a2e854c49cd6cfdddede37344389e21488
pongsoyun <thdbstjdud@gmail.com>
2020-02-11 15:38:00 +0900

    stage1 - 회전바구니 핀 회전

---
+++ TheBible/Assets/Scenes/Stage1/BasketRotation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketRotation : MonoBehaviour
+{
+    [SerializeField]
+
+    [Range(0, 1)]
+    public float rotZ = 0.3f; // default : 0.3f
+
+    [Range(0, 40)]
+    public int rotSpeed = 20; // defalut : 20
+    public bool dirRight = true;
+
+
+    void Update()
+    {
+        Debug.Log(transform.rotation.z);
+        if (transform.rotation.z >= rotZ)
+        {
+            dirRight = false;
+        }
+        else if (transform.rotation.z <= -rotZ)
+        {
+            dirRight = true;
+        }
+
+        if (dirRight == true)
+        {
+            transform.Rotate(new Vector3(0, 0, rotSpeed) * Time.deltaTime);
+        }
+        else
+        {
+            transform.Rotate(new Vector3(0, 0, -rotSpeed) * Time.deltaTime);
+        }
+
+
+    }
+}
