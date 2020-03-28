563539722c3daa222c8db131ad630e1533316dc1
dongho5309 <dongho5309@naver.com>
2020-03-28 21:04:07 +0900

    ThrowObject 확장성 개선, MouseGame 기본세팅

---
+++ TheBible/Assets/Scripts/Singleton/MouseGameManager.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseGameManager : Singleton<MouseGameManager>, IGameProcess
+{
+    public event Action GameStart;
+    public event Action GameComplete;
+    public event Action GameOver;
+
+    [SerializeField]
+    private GameObject StonePrefab;
+    GameState state;
+    public MemoryPool StonePool;
+
+    public int waveLimit;
+    public int killCount;
+
+    void Awake()
+    {
+        GameStart += InitializeGame;
+        GameComplete += GameClear;
+        GameOver += GameFail;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameStart();
+        StonePool = new MemoryPool(StonePrefab, 10, 20);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void InitializeGame()
+    {
+        state = GameState.Start;
+        waveLimit = 40;
+        killCount = 0;
+        StartCoroutine(StoneSpawn());
+    }
+
+    private void GameClear()
+    {
+        state = GameState.Clear;
+        StopCoroutine(StoneSpawn());
+    }
+
+    private void GameFail()
+    {
+        state = GameState.Fail;
+        StopCoroutine(StoneSpawn());
+    }
+
+    IEnumerator StoneSpawn()
+    {
+        while (state.Equals(GameState.OnGoing))
+        {
+            if (killCount < waveLimit)
+            {
+                StonePool.Respawn(transform.position, transform.rotation);
+                yield return new WaitForSeconds(0.2f);
+            }
+            else
+            {
+                Debug.Log("GameEnd!");
+                GameClear();
+                yield return new WaitForEndOfFrame();
+            }
+        }
+
+    }
+}
---
+++ TheBible/Assets/Scripts/StoneMouse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoneMouse : ThrowObject, IDespawnable
+{
+    public event Action<GameObject> OnDespawn;
+
+
+    private void OnMouseDown()
+    {
+        Debug.Log($"{gameObject.name} was Clicked");
+        MouseGameManager.instance.StonePool.Despawn(gameObject);
+        MouseGameManager.instance.killCount++;
+    }
+
+    protected override void PowerSet()
+    {
+        base.PowerSet();
+    }
+}
