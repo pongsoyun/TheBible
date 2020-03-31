0d08301c9688cec876b32e7c3bfbc5e43a07a927
pongsoyun <thdbstjdud@gmail.com>
2020-03-31 16:22:12 +0900

    GameOver - Player Spawn

--- TheBible/Assets/Scripts/Manager/GameManager.cs
+++ TheBible/Assets/Scripts/Manager/GameManager.cs
@@ -11,19 +11,24 @@ public class GameManager : MonoBehaviour
     // stage 1
     public GameObject Player;
     public float gameOverY = -30.0f;
-    
+
+    public Transform Flag1;
+    // public Transform Flag1Down;
+
+    public bool isGameOver;
+
     void Start()
     {
-        // GameEventManager.GameOver = true; // 추후에 상태로 관리할 예정이라면
         GameOverText.enabled = false; // default 설정
+        isGameOver = false;
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        // Game Over 출력
-        if (Player.transform.localPosition.y < gameOverY)
+        // GameOver
+        if (!isGameOver && Player.transform.localPosition.y < gameOverY)
         {
             Debug.Log("gameover");
             GameOver();
@@ -32,6 +37,21 @@ public class GameManager : MonoBehaviour
 
     private void GameOver()
     {
+        isGameOver = true;
         GameOverText.enabled = true;
+        Invoke("Wait3Sec", 3f);
+    }
+
+    private void Wait3Sec()
+    {
+        Debug.Log("restart");
+        Player.transform.position = new Vector3(Flag1.position.x, Flag1.position.y, Flag1.position.z);
+        isGameOver = false;
+        GameOverText.enabled = false;
+
+
+        // 좀 더 부드럽게 가져오려 했는데 우선은,,, 기본구현부터푸쉬,, 
+        // Vector3 flag1Down = new Vector3(Flag1Down.position.x, Flag1Down.position.y, Flag1Down.position.z);
+        // Player.transform.position = Vector3.MoveTowards(transform.position, flag1Down, 2f); 
     }
 }
