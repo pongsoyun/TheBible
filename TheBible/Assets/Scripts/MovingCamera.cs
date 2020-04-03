373b76651d22ee05199c32b9c3eddd7ee22bfdd4
pongsoyun <thdbstjdud@gmail.com>
2020-04-03 22:58:50 +0900

    Camera position.y 고정, Player 세부값 조정

---
+++ TheBible/Assets/Scripts/MovingCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCamera : MonoBehaviour
+{
+    public Transform Player;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Player.position.x + 3.5f, -19.5f, 0);
+    }
+}
