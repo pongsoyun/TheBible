ec57f4d0f15ce58b817d07f5b46dbc58f7df0c4f
pongsoyun <thdbstjdud@gmail.com>
2020-04-10 23:03:58 +0900

    :sparkles: movieSection Animation

---
+++ TheBible/Assets/Scripts/PlayMovie.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMovie : MonoBehaviour
+{
+    public Animator MovieAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Animator = GetComponent<Animator>();
+        MovieAnim.SetBool("play", false);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("play movie~");
+            MovieAnim.SetBool("play", true);
+        }
+    }
+}
