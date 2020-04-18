105661ddc2148600111968b346fe99f7143ab0c8
pongsoyun <thdbstjdud@gmail.com>
2020-04-18 13:04:42 +0900

    :sparkles: MG1도입시 Animation 시작되게, MG2 MainChar Animation추가 부분 코드

---
+++ TheBible/Assets/Scripts/PlayerStoneClick.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStoneClick : MonoBehaviour
+{
+    private void OnCollisionStay2D(Collision2D collision){
+        if(collision.gameObject.CompareTag("Stone2")){
+            // Player 돌맞았을 때 Animation 추가되면 넣을 부분 
+
+        }
+    }
+}
