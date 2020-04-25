2f42586ce28235b4c0b577c1e90d4def8d527113
dongho5309 <dongho5309@naver.com>
2020-04-25 23:19:04 +0900

    #25 #29 시네머신 카메라 무빙 성공

---
+++ TheBible/Assets/ActivateCM.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class ActivateCM : MonoBehaviour
+{
+    [SerializeField]
+    PlayableDirector director;
+    [SerializeField]
+    string tagName;
+
+    bool isPlayOnce = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(!isPlayOnce && collision.CompareTag(tagName))
+        {
+            isPlayOnce = true;
+            director.Play();
+        }
+    }
+}
