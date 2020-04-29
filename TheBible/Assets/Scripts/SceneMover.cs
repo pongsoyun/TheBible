70dbca640c9f12853dba23e3b176f8d9525841ff
pongsoyun <thdbstjdud@gmail.com>
2020-04-29 18:42:23 +0900

    :sparkles: Ending 의심부분 애니메이션 처리

--- TheBible/Assets/Scripts/SceneMover.cs
+++ TheBible/Assets/Scripts/SceneMover.cs
@@ -11,17 +11,6 @@ public class SceneMover : MonoBehaviour
 
     bool sceneStart = false;
     public GameObject MGIntroObjs;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
--- TheBible/Assets/Scripts/SceneSeparator.cs
+++ TheBible/Assets/Scripts/SceneSeparator.cs
@@ -8,9 +8,15 @@ public class SceneSeparator : MonoBehaviour
     PlayableDirector beforeDirector;
     [SerializeField]
     PlayableDirector afterDirector;
+
+    public GameObject MGEndingObjs;
     public bool isBeforeCpt = true;
     private bool isPlayOnceBefore = false;
     private bool isPlayOnceAfter = false;
+    void Start()
+    {
+        MGEndingObjs.SetActive(false);
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!isPlayOnceBefore && isBeforeCpt && collision.CompareTag("Player"))
@@ -21,6 +27,7 @@ public class SceneSeparator : MonoBehaviour
         }
         else if (!isPlayOnceAfter && isPlayOnceBefore && !isBeforeCpt && collision.CompareTag("Player"))
         {
+            MGEndingObjs.SetActive(true);
             isPlayOnceAfter = true;
             afterDirector.Play();
         }
