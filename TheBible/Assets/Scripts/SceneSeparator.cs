449e93a6bddef15108d1b98af9f010022b2c4354
pongsoyun <thdbstjdud@gmail.com>
2020-04-29 18:14:54 +0900

    :sparkles: 카메라 시야관련 제외 cinemachine  구현 완료

---
+++ TheBible/Assets/Scripts/SceneSeparator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+public class SceneSeparator : MonoBehaviour
+{
+    [SerializeField]
+    PlayableDirector beforeDirector;
+    [SerializeField]
+    PlayableDirector afterDirector;
+    public bool isBeforeCpt = true;
+    private bool isPlayOnceBefore = false;
+    private bool isPlayOnceAfter = false;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!isPlayOnceBefore && isBeforeCpt && collision.CompareTag("Player"))
+        {
+            isBeforeCpt = false;
+            isPlayOnceBefore = true;
+            beforeDirector.Play();
+        }
+        else if (!isPlayOnceAfter && isPlayOnceBefore && !isBeforeCpt && collision.CompareTag("Player"))
+        {
+            isPlayOnceAfter = true;
+            afterDirector.Play();
+        }
+    }
+}
