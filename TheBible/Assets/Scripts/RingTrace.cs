0293403d03c475ba6190865107e129f1036f8299
pongsoyun <thdbstjdud@gmail.com>
2020-05-01 00:21:41 +0900

    :zap: 헤일로 detail 수정

---
+++ TheBible/Assets/Scripts/RingTrace.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingTrace : MonoBehaviour
+{
+    public Transform MainChar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, MainChar.position.y + 1.05f, transform.position.z);
+    }
+}
