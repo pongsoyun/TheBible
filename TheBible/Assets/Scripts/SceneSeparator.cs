6cf9f86147b2445afd49692de4c5cee8a896f973
pongsoyun <thdbstjdud@gmail.com>
2020-05-04 23:34:56 +0900

    :sparkles: Ending 부분 애니메이션 처리 - vcam 시야 zone 변경 처리 필요

--- TheBible/Assets/Scripts/SceneSeparator.cs
+++ TheBible/Assets/Scripts/SceneSeparator.cs
@@ -9,14 +9,20 @@ public class SceneSeparator : MonoBehaviour
     [SerializeField]
     PlayableDirector afterDirector;
 
-    public GameObject MGEndingObjs;
+    public GameObject MGEndingRabbits;
+
+    public GameObject ThrowRabbits;
     public bool isBeforeCpt = true;
     private bool isPlayOnceBefore = false;
     private bool isPlayOnceAfter = false;
 
+    public Animator MainCharAnim;
+
     void Start()
     {
-        MGEndingObjs.SetActive(false);
+        MGEndingRabbits.SetActive(false);
+        ThrowRabbits.SetActive(false);
+        MainCharAnim.SetInteger("hurt", 0);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -29,9 +35,35 @@ public class SceneSeparator : MonoBehaviour
         }
         else if (!isPlayOnceAfter && isPlayOnceBefore && !isBeforeCpt && collision.CompareTag("Player"))
         {
-            MGEndingObjs.SetActive(true);
+            MGEndingRabbits.SetActive(true);
             isPlayOnceAfter = true;
             afterDirector.Play();
+            Invoke("EndingAnimPlay", 10f);
         }
+    }
+
+    private void EndingAnimPlay()
+    {
+        // 돌날아오기 + 토끼들 좌우에서 오기
+        ThrowRabbits.SetActive(true);
+
+
+        // MainChar 멍들어가야함
+        Invoke("HurtOne", 5f); // 좌우에서 4초 후 MainChar hurt 1
+        MainCharAnim.SetInteger("hurt", 1);
+        Invoke("HurtTwo", 4f);
+
+        // 시야줄어들기
+
+    }
+
+    private void HurtOne()
+    {
+        MainCharAnim.SetInteger("hurt", 2);
+    }
+
+    private void HurtTwo()
+    {
+        MainCharAnim.SetInteger("hurt", 3);
     }
 }
