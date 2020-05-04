4eb1aeb75f190c662dc73ac1a63d54bd14f5d55f
pongsoyun <thdbstjdud@gmail.com>
2020-05-05 01:34:42 +0900

    :sparkles: Bubble적용완료, MainScene 마지막 AnimSet 적용완료

---
+++ TheBible/Assets/Scripts/BigTalkingBubblePlay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigTalkingBubblePlay : MonoBehaviour
+{
+    public GameObject BigTalkingBubble;
+    // Start is called before the first frame update
+    void Start()
+    {// 6초 후 bubble 활성화
+        Debug.Log("출력력뎢ㅁ러ㅕㅇ니ㅗ랴ㅏ저되놔러");
+        Invoke("Printer", 2f);
+        Invoke("BubbleON", 4f);
+    }
+
+    void BubbleON()
+    {
+        Debug.Log("BubbleON");
+        BigTalkingBubble.SetActive(true);
+    }
+
+    void Printer()
+    {
+        Debug.Log("printer ");
+    }
+}
