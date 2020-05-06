f1205fd4eaa7197da4e27039f0b49d2e68dc3eb3
dongho5309 <dongho5309@naver.com>
2020-05-06 20:04:35 +0900

    CptHome 양 사이드 벽설치, 씬 내부 오브젝트 위치 변경 #33

--- TheBible/Assets/Scripts/CptHomeExit.cs
+++ TheBible/Assets/Scripts/CptHomeExit.cs
@@ -6,12 +6,29 @@ using UnityEngine.SceneManagement;
 public class CptHomeExit : MonoBehaviour
 {
     public Animator KingAnimator;
+    private bool sceneEnd = false;
+    [SerializeField]
+    GameObject Player;
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (KingAnimator.GetBool("cure") && Input.GetKeyDown(KeyCode.W))
+        if (!sceneEnd && KingAnimator.GetBool("cure") && Input.GetKeyDown(KeyCode.W))
         {
-            SceneManager.UnloadSceneAsync(SceneManager.GetSceneByName("CptHome"));
+            sceneEnd = true;
+            Player.GetComponent<CharacterMove>().enabled = false;
+            Invoke("LatePlayerSetFalse", 1f);
+            Invoke("UnloadCptScene", 7f);//7초 뒤에 씬 내리도록
         }
+    }
+
+    private void LatePlayerSetFalse()
+    {
+        Player.SetActive(false);
+    }
+
+    private void UnloadCptScene()
+    {
+        Debug.Log("UnloadScene Called!");
+        SceneManager.UnloadSceneAsync(SceneManager.GetSceneByName("CptHome"));
     }
 }
