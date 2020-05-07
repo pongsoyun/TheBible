fc6bce701a3812c8d41bcc4a4007a5f80c22d7e9
pongsoyun <thdbstjdud@gmail.com>
2020-05-08 00:18:42 +0900

    :rocket: sound Sorted

---
+++ TheBible/Assets/Scripts/SoundPlay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlay : MonoBehaviour
+{
+    private AudioSource musicPlayer;
+    public AudioClip backgroundMusic;
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicPlayer = GetComponent<AudioSource>();
+        playSound(backgroundMusic, musicPlayer);
+    }
+
+    public void playSound(AudioClip clip, AudioSource audioPlayer)
+    {
+        audioPlayer.Stop();
+        audioPlayer.clip = clip;
+        audioPlayer.loop = true;
+        audioPlayer.time = 0;
+        audioPlayer.Play();
+    }
+}
