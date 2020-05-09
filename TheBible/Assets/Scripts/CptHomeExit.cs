84c93f054a2c4be2d296c42eec1acb5294bfb230
pongsoyun <thdbstjdud@gmail.com>
2020-05-10 02:08:47 +0900

    :sparkles: Bubble - Main, CptHouse 포함 전체 말풍선 완료

--- TheBible/Assets/Scripts/CptHomeExit.cs
+++ TheBible/Assets/Scripts/CptHomeExit.cs
@@ -9,15 +9,25 @@ public class CptHomeExit : MonoBehaviour
     private bool sceneEnd = false;
     [SerializeField]
     GameObject Player;
+    [SerializeField]
+    GameObject BigTalkingBubble;
+
+    private void Start()
+    {
+        BigTalkingBubble.SetActive(false);
+    }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (!sceneEnd && KingAnimator.GetBool("cure") && Input.GetKeyDown(KeyCode.W))
         {
+            // 수근수근이수근
             sceneEnd = true;
+            BigTalkingBubble.SetActive(true);
             Player.GetComponent<CharacterMove>().enabled = false;
             Invoke("LatePlayerSetFalse", 1f);
             Invoke("UnloadCptScene", 7f);//7초 뒤에 씬 내리도록
+
         }
     }
 
